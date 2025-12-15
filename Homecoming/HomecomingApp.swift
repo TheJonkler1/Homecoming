990a941f6eeb17f0a6ef95da3b1e7174bdfc012b
Devin J <djoseph7181@stu.d214.org>
2025-12-15 10:44:19 -0600

    Initial Commit

---
+++ Homecoming/HomecomingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomecomingApp.swift
+//  Homecoming
+//
+//  Created by Devin M. Joseph on 12/15/25.
+//
+
+import SwiftUI
+
+@main
+struct HomecomingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
