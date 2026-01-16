996d9026924de44291a9c8b02332c7ef53804b6c
George K <gkoroulis7201@stu.d214.org>
2026-01-16 10:51:58 -0600

    created a function to send an email to a student through the app with the script Leo sent me

--- Homecoming/ScanID.swift
+++ Homecoming/ScanID.swift
@@ -10,6 +10,7 @@ import AVFoundation
 import UIKit
 
 struct ScanID: View {
+    let email = "gkoroulis7201@stu.d214.org"
     var body: some View {
         Text("ScanID")
         
@@ -20,6 +21,22 @@ struct ScanID: View {
                 Text("Camera will go here")
             }
     }
+    
+    func sendEmail() {
+        let subject = "Homecoming Ticket Receipt"
+        let body = "Thank you [student name] for purchasing your [year] Homecoming ticket! \n\n This is a confirmation of your $[price] purchase, placed on [mm/dd/yyyy] at [time]. If this was not you, please contact Ms. Monahan at laura.monahan@d214.org or visit her in the ARC in room 123. \n\n ~ John Hersey High School"
+        
+        let encodedSubject = subject.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+        let encodedBody = body.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+        
+        let mailToString = "mailto:\(email)?subject=\(encodedSubject)&body=\(encodedBody)"
+        
+        if let url = URL(string: mailToString) {
+            UIApplication.shared.open(url)
+        }
+        
+    }
+    
 }
 
 
